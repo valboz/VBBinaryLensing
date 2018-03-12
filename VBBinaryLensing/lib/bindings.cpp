e030b74043a9bdb93298e2ff867826e4c9f68efb
Fran Bartolic <fb90@st-andrews.ac.uk>
2018-03-12 14:32:11 +0000

    Added some test docstrings for the python bindings.

--- VBBinaryLensing/lib/bindings.cpp
+++ VBBinaryLensing/lib/bindings.cpp
@@ -6,18 +6,29 @@ namespace py = pybind11;
 PYBIND11_MODULE(VBBinaryLensingLibrary, m) {
     py::class_<VBBinaryLensing>(m, "VBBinaryLensing")
         .def(py::init())
-        .def("LoadESPLTable", &VBBinaryLensing::LoadESPLTable)
-        .def_readwrite("Tol", &VBBinaryLensing::Tol)
-        .def_readwrite("RelTol", &VBBinaryLensing::RelTol)
-        .def_readwrite("a1", &VBBinaryLensing::a1)
+        .def("LoadESPLTable", &VBBinaryLensing::LoadESPLTable,
+                "Loads pre-calculated table needed for ESPL light curve computation")
+        .def_readwrite("Tol", &VBBinaryLensing::Tol,
+                "Absolute accuracy")
+        .def_readwrite("RelTol", &VBBinaryLensing::RelTol,
+                "Relative accuracy")
+        .def_readwrite("a1", &VBBinaryLensing::a1,
+                "Linear limb darkening coefficient. I(r)=I(0)(1-a1(1-\sqrt{1-r^2/\rho^2}))")
         .def("ESPLMag2", &VBBinaryLensing::ESPLMag2,
-            py::return_value_policy::reference)
+            py::return_value_policy::reference,
+            "ESPLMag2 works the same way as BinaryMag2. It checks whether we are\
+            far enough to use the point-source approximation and if necessary,\
+            it goes for the full computation by calling\
+            ESPLMagDark(double u, double rho, double a1)")
         .def("BinaryMag0",
-            (double (VBBinaryLensing::*)(double, double, double, double)) &VBBinaryLensing::BinaryMag0)
+            (double (VBBinaryLensing::*)(double, double, double, double)) &VBBinaryLensing::BinaryMag0,
+            "Binary light curve computation assuming a point source")
         .def("BinaryMag2", &VBBinaryLensing::BinaryMag2,
-            py::return_value_policy::reference)
+            py::return_value_policy::reference,
+            "Full binary light curve computation")
         .def("PlotCrit", &VBBinaryLensing::PlotCrit,
-            py::return_value_policy::reference);
+            py::return_value_policy::reference,
+            "Returns critical curves and caustic curves.");
     py::class_<_theta>(m, "_theta")
         .def(py::init<double>()); //constructor 
     py::class_<_point>(m, "_point")

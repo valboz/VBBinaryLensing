877eda6526979a37de6ebff57a0891e43d87e7c4
Fran Bartolic <fb90@st-andrews.ac.uk>
2018-03-15 10:53:26 +0000

    Testing if Travis is going to work

--- VBBinaryLensing/tests/test_magnification.py
+++ VBBinaryLensing/tests/test_magnification.py
@@ -11,21 +11,21 @@ def test_ESPL_magnification():
 
     assert np.allclose(magnification, 3./np.sqrt(5.))
 
-def test_amplification_USBL():
-    s = 1.
-    q = 0.02
-    y1 = 0.5 
-    y2 = 0.5 
-    rho = 0.0033
-    VBBL.Tol = 1.e-3
-
-    magnification = VBBL.BinaryMag2(s, q, y1, y2, rho);
-
-    # Reset Tol
-    VBBL.Tol = 0.01
-
-    assert np.allclose(magnification, 1.6311724868)
-
+#def test_amplification_USBL():
+#    s = 1.
+#    q = 0.02
+#    y1 = 0.5 
+#    y2 = 0.5 
+#    rho = 0.0033
+#    VBBL.Tol = 1.e-3
+#
+#    magnification = VBBL.BinaryMag2(s, q, y1, y2, rho);
+#
+#    # Reset Tol
+#    VBBL.Tol = 0.01
+#
+#    assert np.allclose(magnification, 1.6311724868)
+#
 def test_amplification_PSBL():
     s = 1.
     q = 0.02

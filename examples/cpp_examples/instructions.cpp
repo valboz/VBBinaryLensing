61096c6d32cf51765d83c93c55f93a7d384a249c
Valerio Bozza <valboz@sa.infn.it>
2024-01-11 16:00:27 +0100

    Update instructions.cpp

--- examples/cpp_examples/instructions.cpp
+++ examples/cpp_examples/instructions.cpp
@@ -1,4 +1,4 @@
-// VBBinaryLensing v3.1 (2021)
+// VBBinaryLensing v3.6 (2023)
 //
 // This code has been developed by Valerio Bozza (University of Salerno) and collaborators.
 // Any use of this code for scientific publications should be acknowledged by a citation to:
@@ -37,6 +37,7 @@ double u1, u2;
 // - Limb darkening with arbitrary profiles
 // - Point-source binary magnification
 // - Implementation notes on BinaryMag2
+// - Parameters range
 // - Multi-band observations
 // - Extended-source-point-lens magnification
 // - Calculation of full light curves
@@ -97,7 +98,7 @@ int main()
 	// For example, let us set a poor 10% precision, just to see the difference
 	VBBL.RelTol = 1.e-1;
 	Mag = VBBL.BinaryMag2(s, q, y1, y2, Rs); // The result will be Mag*(1 +- VBBL.RelTol) (relative precision)
-	printf("Magnification (relative precision at 1.e-1) = %lf\n", Mag); // Output should be 18.36.... (matching the required precision)
+	printf("Magnification (relative precision at 1.e-1) = %lf\n", Mag); // Output should be 18.24.... (matching the required precision)
 																		// If you do not want to use relative precision anymore, just set
 	VBBL.RelTol = 0;
 	// In practice, the calculation stops when the first of the two goals is reached 
@@ -109,7 +110,7 @@ int main()
 
 	VBBL.astrometry = true; // Set this flag if you want astrometry.
 
-	Mag = VBBL.BinaryMag2(s, q, y1, y2, Rs); 
+	Mag = VBBL.BinaryMag2(s, q, y1, y2, Rs);
 	// The centroid position is now stored in VBBL.astrox1 and VBBL.astrox2
 	printf("Astrometric shift:\ndx1 = %lf\ndx2 = %lf\n", VBBL.astrox1 - y1, VBBL.astrox2 - y2); // Output should be dx1 = -0.1645... dx2 = -0.0743...
 
@@ -210,7 +211,7 @@ int main()
 	// BinaryMag2 first calculates the point-source magnification through BinaryMag0
 	// Then it evaluates the quadrupole correction. If it is too high, it goes for the full computation.
 	// This is performed by calling BinaryMagDark. This function is also directly available to the user (as it was in v1.0).
-	// Mag = VBBL.BinaryMagDark(s, q, y1, y2, Rs, a1, accuracy); // Note that the limb darkening and the accuracy are required parameters.
+	// Mag = VBBL.BinaryMagDark(s, q, y1, y2, Rs, accuracy); // Note that the accuracy is a required parameter.
 
 	// BinaryMagDark performs contour integration on several annuli. The number and radii of annuli is determined dynamically.
 	// Each contour is treated as a uniform source by a call to the function BinaryMag (also directly available to the user).
@@ -219,6 +220,20 @@ int main()
 	// The use of BinaryMag2 is strongly recommended, since it may save computational time by a factor ranging from 10 to 100.
 	// Useless contour integrations are avoided if the required precision and accuracy are already met 
 	// by the point-source computation.
+
+	//////////////////////////////////////////
+	// Parameters range
+	//////////////////////////////////////////
+
+	// Testing has been performed with 1.e-9 <= q <= 1
+	// Failures (errors larger than declared Tolerance) are below 1 in 1000 caustic crossings in the ranges
+	// 0.01< s < 10 for q=1.0
+	// 0.1 < s < 4 for q=1.e-9
+	// y1 and y2 may vary from 0 to 500 without any failures.
+	// Above ranges apply to source radius between 1.e-3 to 1.0. 
+	// Outside this range the robustness gradually degrades, 
+	// but we typically obtain very good results for reasonable values of s and q.
+
 
 	//////////////////////////////////////////
 	// Multi-band observations
@@ -243,16 +258,17 @@ int main()
 
 	double u = 0.1; // Source-lens separation in Einstein radii.
 	Mag = VBBL.ESPLMag2(u, Rs); // Call to the ESPLMag2 function with these parameters
-	printf("\nMagnification of Extended-source-point-lens = %lf\n", Mag);  // Output should be 10.05.....
+	printf("\nMagnification of Extended-source-point-lens = %lf\n", Mag);  // Output should be 10.049.....
 
-    // Astrometry: the one-dimensional centroid shift is stored in VBBL.astrox1.
+	// Astrometry: the one-dimensional centroid shift is stored in VBBL.astrox1.
 	// (remember that there is only radial shift and no tangential shift in the single-lens case).
 
 	// Implementation notes:
 	// ESPLMag2 works the same way as BinaryMag2. It checks whether we are far enough to use the point-source approximation.
-	// If necessary, it goes for the full computation by calling ESPLMagDark(double u, double rho, double a1);
+	// If necessary, it goes for the full computation by calling ESPLMagDark(double u, double rho);
 	// ESPLMagDark divides the source disk in annuli. 
 	// Each annulus is calculated by a call to ESPLMag(double u, double rho), which uses a pre-calculated table (extremely fast).
+	// The current range for the pre-calculated table is 1.e-4 < Rs < 1.e+2
 	// Default limb darkening is linear law.
 	// You may change the profile using SetLDprofile as explained above.
 
@@ -428,12 +444,28 @@ int main()
 	// BinSourceLightCurveParallax(double *parameters, double *t_array, double *mag_array, double *y1_array, double *y2_array, int np);
 	// Parameters are {log_tE, log_fluxratio, u0_1, u0_2, t0_1, t0_2, pai1, pai2}
 
-	// BinSourceLightCurveXallarap(double *parameters, double *t_array, double *mag_array, double *y1_array, double *y2_array, double *sep_array, int np);
+	// BinSourceSingleLensXallarap(double *parameters, double *t_array, double *mag_array, double *y1_array, double *y2_array, double *y1_array2, double *y2_array2, double *sep_array, int np);
+	// Parameters are {u0, t0, log_tE, log_Rs, xi1, xi2, omega, inc, phi, log_qs}
+	// xi1, xi2 are the xallarap parameters (see Rahvar, Dominik, 2008)
+	// The orbital radius in Einstein radius units is sqrt(xi1*xi1+xi2*xi2)
+	// omega is 2\pi/period (in days^-1)
+	// inc, phi are inclination of the orbit and phase of the primary from the line of nodes
+	// qs is the mass ratio between the two sources
+	// Note that the radius and the flux of the second source are scaled by qs^0.89 and qs^4 respectively.
+	// We are assuming that both sources are main sequence stars of solar mass.
+
+	// BinSourceBinLensXallarap(double* pr, double* ts, double* mags, double* y1s, double* y2s, int np)
+	// Parameters are {log_s, log_q, u0, alpha, log_Rs, log_tE, t0, xi1, xi2, omega, inc, phi, log_qs}
+	// See above for the explanation of xallarap parameters.
+
+	// BinSourceLightCurveXallarap(double *parameters, double *t_array, double *mag_array, double *y1_array, double *y2_array, double *sep_array, int np); 
+	///// Old alternative version
 	// Parameters are {log_tE, log_fluxratio, u0_1, u0_2, t0_1, t0_2, pai1, pai2, q, w1, w2, w3}
 	// Note that the mass ratio q between the two sources is required to calculate orbital motion.
 	// Orbital motion is assumed without eccentricity (see before).
 	// The parameters u0_1, u0_2, t0_1, t0_2 specify the configuration at time t0 calculated as the closest approach of the
 	// center of mass.
+
 
 	// Old (v1) light curve functions calculating the magnitude at one specific time
 
@@ -449,6 +481,8 @@ int main()
 	//double BinSourceLightCurve(double *parameters, double t);
 	//double BinSourceLightCurveParallax(double *parameters, double t);
 	//double BinSourceLightCurveXallarap(double *parameters, double t);
+	//double BinSourceBinLensXallarap(double *parameters, double t);
+	//double BinSourceSingleLensXallarap(double *parameters, double t);
 
 	// These functions work as the new ones but for a single time t at once. 
 	// The magnification is returned through the function name.
@@ -496,6 +530,7 @@ int main()
 	//fclose(f);
 	// Note that critical curves and caustics are in the same list Mycurves. 
 	// If the list contains 2n curves, the first n are the critical curves and the second n curves are the caustics.
+	// The number of points is given by 4*VBBL.NPcrit. The default  value is 200.
 
 
 	//////////////////////////////////////////
@@ -511,7 +546,7 @@ int main()
 // Note that the parameters u1 and u2 are defined as global parameters at the beginning of the file.
 double MyLDprofile(double r) {
 	double costh;
-	costh = sqrt(1 - r*r);
+	costh = sqrt(1 - r * r);
 	return (1 - u1 * (1 - costh) - u2 * (1 - sqrt(costh))); // square root
 //	return (1 - u1 * (1 - costh) - u2 * (1 - costh)*(1 - costh));  // quadratic
 //	return (1 - u1 * (1 - costh) - u2 * costh*log(costh+1.e-9)); // log
